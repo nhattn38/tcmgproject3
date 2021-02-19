fe82ac59c0d4a7251f1d3d9fac9e9b10fcdc561b
Nhat-Tien Nguyen <nhattn38@gmail.com>
2021-02-19 17:41:29 -0600

    creating function

--- solo.py
+++ solo.py
@@ -1,3 +1,9 @@
 import urllib.request
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 urllib.request.urlretrieve(url,'./http_access_log.txt')
+
+file = open("http_access_log.txt")
+data = file.read()
+pastyear = data.count('1994')
+
+print ("This is the total requests made in the last year: ",pastyear)
