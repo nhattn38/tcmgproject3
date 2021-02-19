be2e77ce2b0b9c089e1cc6cfbc978e5411306ece
Nhat-Tien Nguyen <nhattn38@gmail.com>
2021-02-19 17:59:02 -0600

    added more functions

--- solo.py
+++ solo.py
@@ -5,5 +5,17 @@ urllib.request.urlretrieve(url,'./http_access_log.txt')
 file = open("http_access_log.txt")
 data = file.read()
 pastyear = data.count('1994')
+all = data.count("1995")
+total = pastyear + all
 
 print ("This is the total requests made in the last year: ",pastyear)
+
+answer = input ("This program will show the number of requests from a web server. Would you like to see them? (yes or no)")
+if answer == "yes":
+    answer = input("Do you want the number of requests in the past year (past) or total number of requests (total)?")
+    if answer == "past":
+            print ("There have been" + pastyear + "requests in the past year")
+    elif answer == "total":
+            print ("There have been" + total + "requests in total")
+elif answer == "no":
+    print ("Thank you for your time")
